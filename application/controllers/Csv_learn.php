d964d77d12f54df8c67283d9b85ca9a06f8e88f9
Thanhf Dev <thanhfgit@gmail.com>
2018-10-02 00:08:08 +0700

    csv

--- application/controllers/Csv_learn.php
+++ application/controllers/Csv_learn.php
@@ -10,18 +10,39 @@ class Csv_learn extends CI_Controller {
 
     public function csv($a='')
     {
-        if($a != '') {
-            // get file csv
-            header('Content-type:text/csv');
-            header('Content-Dis:text/csv');
-        }
         $data['title'] = 'Csv@easy';
         $data['content'] = 'csv_learn';
 
         $this->load->model('order_detail_model');
         $data['orders'] = $this->order_detail_model->get_orders();
 
+        if($a != '') {
+            // get file csv
+            $filename = 'csv_dev_'.date('YmdH_i_s').'.csv';
+            header('Content-type:text/csv');
+            header('Content-Disposition: attachment;filename='. $filename);
+            header('Cache-Control: no-store, no-cache, must-revalidate');
+            header('Cache-Control: post-check=0,pre-check=0');
+            header('Pragma: no-cache');
+            header('Expires: 0');
+
+            $handle = fopen('php://output','w');
+            fputcsv($handle,array(
+                'ID',
+                'UserID',
+                'Create Date',
+                'Name User',
+                'Ship Date'
+            ));
+
+            foreach ($data['orders'] as $row) {
+                fputcsv($handle,$row);
+            }
+            fclose($handle);
+            exit();
+        }
         $this->load->view('master',$data);
+
 	}
 
 }
