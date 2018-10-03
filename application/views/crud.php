81a0cfdfbc78fdc11d451af03f3e03046b29a123
Thanhf Dev <thanhfgit@gmail.com>
2018-10-03 22:04:47 +0700

    done layouts crud

--- application/views/crud.php
+++ application/views/crud.php
@@ -4,26 +4,90 @@
             <thead>
             <tr>
                 <th scope="col">ID</th>
-                <th scope="col">UserID</th>
-                <th scope="col">Create Date</th>
-                <th scope="col">Username</th>
-                <th scope="col">Ship_at</th>
+                <th scope="col">ProductID</th>
+                <th scope="col" style="width: 20%">Url</th>
+                <th scope="col" style="width: 20%">Alt</th>
+                <th scope="col">Order</th>
+                <th scope="col">Active</th>
+                <th scope="col">Options</th>
             </tr>
             </thead>
             <tbody>
-            <?php foreach ($orders as $order):?>
-            <tr>
-                <th scope="row"><?=$order['id']?></th>
-                <td><?=$order['user_id']?></td>
-                <td><?=$order['name']?></td>
-                <td><?=$order['create_at']?></td>
-                <td><?=$order['ship_at']?></td>
-            </tr>
-            <?php endforeach;?>
+
+                <tr>
+                    <th scope="row">sss</th>
+                    <td>assas</td>
+                    <td>errer</td>
+                    <td>iooi</td>
+                    <td>pppp</td>
+                    <td>qqqqq</td>
+                    <td>
+                        <a class="btn btn-warning" href="#" role="button" data-backdrop="static" data-keyboard="false" data-toggle="modal" data-target="#edit-confirm">Edit</a>
+                        <a class="btn btn-danger" href="#" role="button" id="delete" data-toggle="modal" data-target="#delete-confirm">Delete</a>
+                    </td>
+                </tr>
+
             </tbody>
         </table>
     </div>
 </div>
 <div class="row justify-content-center">
-    <a class="btn btn-primary" href="<?=site_url(array('csv_learn','csv',123))?>" role="button">Csv</a>
+    <a class="btn btn-success mr-2" href="#" role="button">Show more</a>
+    <a class="btn btn-info" href="#" role="button">GoToHome</a>
 </div>
+
+
+<!-- Modal Edit-->
+<div class="modal fade" id="edit-confirm" tabindex="-1" role="dialog" aria-labelledby="edit-confirm" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-primary">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Info</h5>
+                <button type="button" class="close btn-xx" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true" style="position: absolute;top: 7px; left: 9px;">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="recipient-name" class="col-form-label">Recipient:</label>
+                        <input type="text" class="form-control" id="recipient-name">
+                    </div>
+                    <div class="form-group">
+                        <label for="message-text" class="col-form-label">Message:</label>
+                        <textarea class="form-control" id="message-text"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" data>Send message</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--Delete Modal-->
+
+
+<!-- Modal -->
+<div class="modal fade" id="delete-confirm" tabindex="-1" role="dialog" aria-labelledby="delete-confirm" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-danger">
+                <h5 class="modal-title">Delete</h5>
+                <button type="button" class="close btn-xx" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true" style="position: absolute;top: 7px; left: 9px;">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Are you sure delete it ?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger btn-outline-danger">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
+
--- application/views/master.php
+++ application/views/master.php
@@ -6,14 +6,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
     <base href="<?=base_url()?>">
 	<title><?=isset($title) ? $title : '@App | Simple'?></title>
-    <link rel="stylesheet" href="public/bootstrap.css">
+    <link rel="stylesheet" href="public/css/bootstrap.css">
+    <link rel="stylesheet" href="public/css/toastr.css">
+    <style>
+        .btn-xx {
+            background: orange !important;
+            width: 12px;
+            height: 12px;
+            position: relative;
+            top: 7px;
+            border-radius: 6px;
+            left: -6px;
+            box-shadow: cyan -2px 2px;
+        }
+
+    </style>
 </head>
 <body>
 
 <div id="container">
 	<?php $this->load->view($content); ?>
 </div>
-<script src="public/jquery.js"></script>
-<script src="public/bootstrap.js"></script>
+
+<script src="public/js/jquery-3.3.1.js"></script>
+<script src="public/js/bootstrap.js"></script>
+<script>
+    $(function () {
+
+
+    });
+
+</script>
 </body>
 </html>
